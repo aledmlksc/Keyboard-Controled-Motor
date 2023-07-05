e40a9570877036529fdaf522e5adf223b8d94b63
aledmlksc <104858735+aledmlksc@users.noreply.github.com>
2023-07-05 14:23:46 +0200

    Add files via upload

--- arduino keyboard control/arduino_keyboard_motor_controller.ino
+++ arduino keyboard control/arduino_keyboard_motor_controller.ino
@@ -1,16 +1,17 @@
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT); //the pin that the motor is connected to + GND
 }
 
 void loop() {
-  while(Serial.available()){
-  char input = Serial.read();
+  while(Serial.available()) {
+  char input = Serial.read(); //reading the character sent serialy from the key.py
   if (input=='w'){
-    digitalWrite(13, HIGH);
+    digitalWrite(13, HIGH); //if it is the right command the motor rotates
   }
   else{
-    digitalWrite(13, LOW);
+    digitalWrite(13, LOW); //if wrong one the motor is stopped
   }
     
              
